8e6e949c66ea2fb7abbb8e71e97fceb42c080c2c
Nils <nilsgrimmer@gmx.de>
2022-12-11 15:46:31 +0100

    Optimization

--- src/NGExampleDatabaseSchema.cpp
+++ src/NGExampleDatabaseSchema.cpp
@@ -9,12 +9,12 @@
 
 NGExampleDatabaseSchema::NGExampleDatabaseSchema(char* location, NGCustomDataStorage* storage) {
     _create(location, storage);
-    tabOne = new NGExampleDatabaseTable("One");
-    _db->registerTable("One.csv",tabOne);
-    tabTwo = new NGExampleDatabaseTable("Two");
-    _db->registerTable("Two.csv",tabTwo);
 }
 
 void NGExampleDatabaseSchema::_create(char* location, NGCustomDataStorage* storage) {
     NGCustomDatabaseSchema::_create(location, storage);
+    tabOne = new NGExampleDatabaseTable("One");
+    _db->registerTable("One.csv",tabOne);
+    tabTwo = new NGExampleDatabaseTable("Two");
+    _db->registerTable("Two.csv",tabTwo);
 }
